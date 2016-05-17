f68279a2d3d53aa6d7213eaefe93a5f226585b18
buzzerbeat <cancerxp@126.com>
2016-05-17 12:02:48 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=qsyk_160516',
+    'dsn' => 'mysql:host=localhost;dbname=qsyk',
     'username' => 'root',
     'password' => 'my,YZWX;87',
     'charset' => 'utf8',
