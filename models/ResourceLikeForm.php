8cda5b9254341eed96ead2a0dca4bfb5ce46a5a9
buzzerbeat <cancerxp@126.com>
2016-05-20 16:47:55 +0800

    fix like num

--- models/ResourceLikeForm.php
+++ models/ResourceLikeForm.php
@@ -47,13 +47,34 @@ class ResourceLikeForm extends Model
                 'time'=>time(),
             ]);
             $resourceCount = $resourceLike->resourceCount;
+            if (empty($resourceCount)) {
+                $resourceCount = new ResourceCount();
+                $resourceCount->setAttributes([
+                        'resource_id'=>$this->getId(),
+                        'resource_hits'=>0,
+                        'resource_hits_daily'=>0,
+                        'resource_hits_week'=>0,
+                        'resource_post'=>0,
+                        'resource_post_daily'=>0,
+                        'resource_post_week'=>0,
+                        'resource_pre_dig'=>0,
+                        'resource_dig'=>0,
+                        'resource_bury'=>0,
+                        'resource_collect'=>0,
+                        'resource_share'=>0,
+                        'resource_share_click'=>0,
+                ], false);
+                if (!$resourceCount->save()){
+                    $this->addErrors($resourceLike->getErrors());
+                    return false;
+                }
+            }
             if ($resourceCount) {
                 if ($status == ResourceLike::STATUS_LIKE) {
                     $resourceCount->updateCounters(['resource_dig' => 1]);
                 } else if ($status == ResourceLike::STATUS_LIKE) {
                     $resourceCount->updateCounters(['resource_bury' => 1]);
                 }
-
             }
             if (!$resourceLike->save()) {
                 $this->addErrors($resourceLike->getErrors());
